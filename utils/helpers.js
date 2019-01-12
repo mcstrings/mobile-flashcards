8d24a8a044a853e5f654ea0da27a344fb0db0fa1
mcstrings <garyarthuryoung@gmail.com>
2019-01-12 10:35:13 -0500

    feat: Change notification time

--- utils/helpers.js
+++ utils/helpers.js
@@ -52,7 +52,7 @@ export function setLocalNotification() {
 
                             let tomorrow = new Date()
                             tomorrow.setDate(tomorrow.getDate() + 1)
-                            tomorrow.setHours(20)
+                            tomorrow.setHours(8)
                             tomorrow.setMinutes(0)
 
                             Notifications.scheduleLocalNotificationAsync(
